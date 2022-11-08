5af567980136a228b3d4557e635708395461b33c
Abdulkbk <abdoollahikbk@gmail.com>
2022-11-08 10:19:53 +0100

    Signup & Signin pages

---
+++ src/Components/Auth/SignIn.tsx
@@ -0,0 +1,93 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  Checkbox,
+} from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { LockOutlined } from '@mui/icons-material'
+
+const theme = createTheme()
+
+function SignIn() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Sign in
+          </Typography>
+          <Box component='form' noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <FormControlLabel
+              control={<Checkbox defaultChecked color='primary' />}
+              label='Remeber me'
+              defaultChecked={false}
+              sx={{ width: '100%', mt: 1 }}
+            />
+
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs sx={{ textAlign: 'left' }}>
+                <Link href='#' variant='body2'>
+                  Forgot password
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href='#' variant='body2'>
+                  {'Sign Up'}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default SignIn
---
+++ src/Components/Auth/SignUp.tsx
@@ -0,0 +1,94 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme()
+
+function SignUp() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <Container component='main' maxWidth='xs'> */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          width: '100vw',
+          // bgcolor: 'green',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} />
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
+        <Box
+          component='form'
+          sx={{ mt: 1, width: '30%', minWidth: '300px', px: 1 }}
+        >
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='name'
+            label='Full Name'
+            name='name'
+            autoComplete='name'
+            size='small'
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            size='small'
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='password'
+            label='Password'
+            name='password'
+            size='small'
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='password'
+            label='Confirm Password'
+            name='confirm_password'
+            size='small'
+          />
+
+          <Button type='submit' fullWidth variant='contained' sx={{ mt: 2 }}>
+            Sign up
+          </Button>
+          <Grid container>
+            <Grid item marginTop={2}>
+              <Link>{'Already have an account? Sign in'}</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      {/* </Container> */}
+    </ThemeProvider>
+  )
+}
+
+export default SignUp
